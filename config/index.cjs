8a60fc07c6a67c53afa1d8b602140cf66a395235
崔伟 <42238911+itry1997@users.noreply.github.com>
2023-05-14 14:11:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '臭臭',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oJ7Zj6qhyxBJ62nOyactv77VrCyI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -42,9 +42,9 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-08-15' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2022-08-15' },
       ],
     },
   ],
