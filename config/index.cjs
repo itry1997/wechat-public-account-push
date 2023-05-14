2a4f8f9ef54d839cbdde36b8e264d618e051559b
崔伟 <42238911+itry1997@users.noreply.github.com>
2023-05-14 14:35:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,14 +29,17 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1997', date: '02-04',
+          type: '*生日', name: '臭臭', year: '1997', date: '02-04',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1997', date: '03-12',
+          type: '生日', name: '臭臭', year: '1997', date: '03-12',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '生日', name: '少爷', year: '1997', date: '11-26',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '08-15',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
